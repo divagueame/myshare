7bf2fe10a6ee1b12393893b73227b784126c1d0d
Divagueame <martin.arce.teixeira@gmail.com>
2022-08-24 22:07:21 +0200

    Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,7 +59,6 @@ class PostsController < ApplicationController
     end
 
     def post_params
-      # params.require(:post).permit(:title, :body, :image)
       params.require(:post).permit(:title, :body, images: [])
     end
 end
